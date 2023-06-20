31ee8630d28455fb4633faedd2428afa50683a94
Pinkylml <65669830+Pinkylml@users.noreply.github.com>
2023-06-20 08:00:42 -0500

    Add files via upload
    Ejemplo en clase 20/06/2023 Martes, codigo de morfosis de cuadrado a circulo

---
+++ Morfosis/Morphing.c
@@ -0,0 +1,68 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <GL/freeglut.h>
+#include <cmath>
+
+int refreshRate = 300;
+int segNum = 4;  //porque empesamos con un cuadrado
+int radio = 50;
+
+
+void reshape(GLint w, GLint h);
+void initGL();
+void timer(int valor);
+void morphing(int segmentos);
+void displayMorph();
+
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(600, 480);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Ejercicio MOrphing");
+	glutDisplayFunc(displayMorph);
+	glutReshapeFunc(reshape); //eviat que se redibuje al aumentar la ventana, no es necesario esta funcion.
+	glutTimerFunc(0, timer, 0);
+	initGL();
+	glutMainLoop();//Mantener abierta la ventana.
+
+}
+
+void reshape(GLint w, GLint h) {
+	glViewport(0, 0, w, h);
+	//glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//gluPerspective(60.0f, 1, 5, 100);	//La perspectiva esta entre 1 y 5
+	gluOrtho2D(-100, 100, -100, 100);
+}
+
+void initGL() {
+	glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+}
+
+void timer(int valor) {
+	glutPostRedisplay(); //Fuerza un recibo
+	glutTimerFunc(refreshRate, timer, 0);
+}
+
+void displayMorph() {
+	glClear(GL_COLOR_BUFFER_BIT);
+		morphing(segNum);
+		glutSwapBuffers();
+	segNum++;
+}
+
+void morphing(int segmentos) {
+	float theta = 0.0f;
+	float x = 0.0f;
+	float y = 0.0f;
+	glBegin(GL_POLYGON);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	for (int i = 0; i < segmentos; i++) {
+		theta = M_PI * 2.0F * float(i) / float(segmentos);
+		x = float(radio) * cosf(theta - (M_PI * 45.0F / 180.0F));
+		y = float(radio) * sinf(theta - (M_PI * 45.0F / 180.0F));
+		glVertex2f(x, y);
+	}
+	glEnd();
+}
